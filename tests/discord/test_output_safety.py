cf5a2a1b426c321deb949ddeb6ff6da5689b5101
sendoru <sendol39@gmail.com>
2026-09-17 14:21:11 +0900

    Test user mention output policy

--- tests/discord/test_output_safety.py
+++ tests/discord/test_output_safety.py
@@ -4,11 +4,15 @@ from hina_bot.discord.output_safety import DISCORD_MENTION, neutralize_mentions
 
 
 class OutputSafetyTests(unittest.TestCase):
-    def test_every_discord_notification_form_is_neutralized(self):
-        unsafe = "@everyone @here <@123> <@!456> <@&789>"
+    def test_broadcast_and_role_mentions_are_neutralized(self):
+        unsafe = "@everyone @here <@&789>"
         result = neutralize_mentions(unsafe)
         self.assertIsNone(DISCORD_MENTION.search(result))
-        self.assertEqual(result, "＠everyone ＠here <＠123> <＠!456> <＠&789>")
+        self.assertEqual(result, "＠everyone ＠here <＠&789>")
+
+    def test_user_mentions_are_preserved(self):
+        text = "<@123> <@!456> 안녕"
+        self.assertEqual(neutralize_mentions(text), text)
 
     def test_normal_text_and_custom_emoji_are_unchanged(self):
         text = "안녕 @user <:hina_happy:1234> 이메일 user@example.com"
